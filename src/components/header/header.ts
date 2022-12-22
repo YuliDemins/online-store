5987d4ca21a32dee158618196da277f42322877d
Yulia <yudemins@mail.ru>
2022-12-22 22:14:54 +0300

    refactor: remove comments in header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,5 +1,3 @@
-// import {createElement} from '../../utils';
-
 import { BaseComponent } from '@/services/BaseComponent';
 
 export class Header extends BaseComponent {
@@ -13,44 +11,13 @@ export class Header extends BaseComponent {
 
   private mainSearchInput;
 
-  cart: BaseComponent;
+  private cart;
 
-  cartShopping: BaseComponent;
+  private cartShopping;
 
-  cartShoppingTitle: BaseComponent;
-
-  // mainSearch: HTMLElement;
-
-  // mainSearchInput: HTMLElement;
-
-  // mainSearchBtn: HTMLElement;
-
-  // cart: HTMLElement;
-
-  // cartShopping: HTMLElement;
-
-  // cartShoppingTitle: HTMLElement;
+  private cartShoppingTitle;
 
   constructor() {
-    // this.header = createElement('header', 'header');
-    // this.wrapper = createElement('div', 'wrapper');
-    // this.title = createElement('h1', 'title', 'Store');
-
-    // this.mainSearch = createElement('form', 'main-search');
-    // this.mainSearchInput = createElement('input', 'main-search-input');
-    // this.mainSearchInput.setAttribute('type', 'text');
-    // this.mainSearchInput.setAttribute('placeholder', 'Поиск по товарам');
-    // this.mainSearchBtn = createElement('button', 'main-search-btn');
-    // this.mainSearch.append(this.mainSearchInput, this.mainSearchBtn);
-
-    // this.cart = createElement('div', 'cart');
-    // this.cartShopping = createElement('div', 'cart-shopping');
-    // this.cartShoppingTitle = createElement('div', 'cart-shopping-title', 'Корзина');
-
-    // this.cart.append(this.cartShopping, this.cartShoppingTitle);
-    // this.wrapper.append(this.title, this.mainSearch, this.cart);
-    // this.header.append(this.wrapper);
-
     super({
       tag: 'header',
       className: 'header',
