de3e896f2c9cb81fd5535b867c5b6b7336e1d115
SlikeDollar <mibonbon73@gmail.com>
2022-12-22 16:59:24 +0100

    refactor: move header to classes

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,24 +1,32 @@
 // import { root } from "../../index";
-import { createElement } from '../../utils';
+import { BaseComponent } from '@/services/BaseComponent';
 
-export class Header {
-  header: HTMLElement;
+export class Header extends BaseComponent {
+  private wrapper;
 
-  wrapper: HTMLElement;
-
-  title: HTMLElement;
-
-  root: HTMLElement | null;
+  private title;
 
   constructor() {
-    this.header = createElement('div', 'header');
-    this.wrapper = createElement('div', 'wrapper');
-    this.title = createElement('h1', 'title', 'Store');
-    this.wrapper.append(this.title);
-    this.header.append(this.wrapper);
+    super({
+      tag: 'header',
+      className: 'header',
+    });
 
-    this.root = document.getElementById('root');
-    this.root?.append(this.header);
+    this.wrapper = new BaseComponent({
+      tag: 'div',
+      className: 'header__wrapper',
+    });
+
+    this.title = new BaseComponent({
+      tag: 'h1',
+      className: 'title',
+      textContent: 'Store',
+    });
+  }
+
+  render() {
+    this.wrapper.addChildren(this.title.elem);
+    this.addChildren(this.wrapper.elem);
   }
 }
 
